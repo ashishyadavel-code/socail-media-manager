f3c31bd369f9dd31a4f353cf03cd4cf97b728722
Ashish Yadav <ashishyadavel@gmail.com>
2026-03-28 00:09:47 +0530

    Updated project files

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,20 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xb*j4l(3w=y_omkrhtwv@f&w#yq7xtu%_f%0p)h%dz83w2z43#'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-xb*j4l(3w=y_omkrhtwv@f&w#yq7xtu%_f%0p)h%dz83w2z43#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = []
 
